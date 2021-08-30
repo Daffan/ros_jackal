73bd73ac8e46c144afa53fa6de938dcc87053715
Zifan Xu <zifanxu96@gmail.com>
2021-08-30 17:14:57 -0500

    fix collect habitat test

--- scripts/collect_habitat_test.py
+++ scripts/collect_habitat_test.py
@@ -15,13 +15,14 @@ if __name__ == "__main__":
         for l in f.readlines():
             l = l.split(" ")
             h = int(l[0])
-            time[h].append(float(l[2])) if int(l[-1])
+            if int(l[-1]):
+                time[h].append(float(l[2]))
+                recovery[h].append(int(l[-2]))
             success[h].append(int(l[-1]))
-            recovery[h].append(int(l[-2]))
     avg_time = []
     avg_success = []
     for k in sorted(time.keys()):
-        print("habitat: %d, time: %.4f, success: %.2f" %(k, np.mean(time[k]), np.mean(success[k])))
+        print("habitat: %d, time: %.4f, success: %.2f, (%d/%d)" %(k, np.mean(time[k]), np.mean(success[k]), len(time[k]), len(success[k])))
         avg_time.append(np.mean(time[k]))
         avg_success.append(np.mean(success[k]))
     print("Average time: %.4f, average success: %.2f" %(np.mean(avg_time), np.mean(avg_success)))
