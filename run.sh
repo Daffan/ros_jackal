e8dc59d93a2c39dfb93afb400dc1df6705987ec3
Bo Liu <bliu0201@gmail.com>
2021-10-09 15:07:23 -0500

    update script

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo $pwd
 
 if [[ $1 -eq 1 ]]
 then
-    export BUFFER_PATH=$pwd/buffer_folder && ./singularity_run.sh python3 td3/train.py --config_path configs/motion_laser.yaml
+    export BUFFER_PATH=$pwd/buffer_folder && ./singularity_run.sh python3 td3/train.py --config_path ./configs/motion_laser.yaml
 else
-    export BUFFER_PATH=$pwd/buffer_folder && python3 scripts/train_condor.py --local_update --config_path configs/motion_laser.yaml
+    export BUFFER_PATH=$pwd/buffer_folder && python3 scripts/train_condor.py --local_update --config_path ./configs/motion_laser.yaml
 fi
 
